313614163361fdf0eefa5282d94b58040bc1c98a
pauloeduardo145 <pauloeduardodasilvaporto613@gmail.com>
2026-05-30 13:47:38 -0300

    começando concerto do frame inicio

--- interface.py
+++ interface.py
@@ -2,16 +2,19 @@ import customtkinter as ctk
 
 ctk.set_appearance_mode("dark")
 
-janela = ctk.CTk()
+#JANELAS
 
+janela = ctk.CTk()
 janela.title("Criador de Mundos")
 janela.geometry("1000x1000")
-
 janela.attributes('-fullscreen', True)
-
 janela.bind('<Escape>', lambda e: janela.attributes('-fullscreen', False))
-
 janela.grid_columnconfigure(1, weight=1)
+
+#INICIO
+
+frame_inicio = ctk.CTkFrame(janela, width=300, height=500)
+frame_inicio.grid(row=0, column=0, sticky="ns", padx=10, pady=10)
 
 inicio = ctk.CTkLabel(janela,text="INICIO")
 inicio.grid(row=1, column=0, padx=100,pady=90)
